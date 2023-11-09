712a1333bbd8a0a2ec53aae8ac2c4492e88ba43a
noa <banoa@post.bgu.ac.il>
2023-11-09 17:25:05 +0200

    player tiuta

---
+++ player.java
@@ -0,0 +1,17 @@
+public class player {
+
+    private String name = "";
+    private int score = 0;
+    private String country = "";
+    private String city = "";
+    private String creature = "";
+    private String plant = "";
+    private String object = "";
+
+    
+    public void printPlayer(){
+        System.out.println(name);
+    };
+
+
+}
